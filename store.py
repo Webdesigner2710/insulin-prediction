ad890ce1f20f38ca7c7a4b37294054ea088e7a47
Aditya Sandeep Kamat <65214290+Webdesigner2710@users.noreply.github.com>
2024-03-17 19:22:45 +0530

    Add files via upload

---
+++ store.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+
+# Load data
+def load_data():
+    return pd.read_excel('insulin information.xlsx')
+
+store_data = load_data()
+
+# Streamlit app
+def main():
+    # HTML for gradient background
+    st.markdown("""
+        <style>
+        .reportview-container {
+            background: linear-gradient(to right, #4ca1af, #c4e0e5);
+            color: black;
+        }
+        table.dataframe {
+            width: 100%;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    # Title
+    st.title('Store Locator')
+
+    # Sidebar - Brand Name input
+    brand_name = st.sidebar.text_input("Enter Brand Name")
+
+    # Filter data based on brand name
+    filtered_data = store_data[store_data['Brand Name'] == brand_name]
+
+    # Display filtered data
+    if not filtered_data.empty:
+        st.subheader(f'Stores selling {brand_name}:')
+        # Display Store Name, Pincode, Address, Insulin Type, Price, and Quantity Available
+        st.dataframe(filtered_data[['Store Name', 'Address', 'Store Pincode', 'Insulin Type', 'Price (INR)', 'Quantity available']])
+    else:
+        st.write(f"No store found for the brand name '{brand_name}'.")
+
+if __name__ == '__main__':
+    main()
